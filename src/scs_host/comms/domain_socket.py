f7b5b0f4d409e9797fe3c38e467ccacf4a503ab6
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-07-24 13:44:58 +0100

    Fixed DomainSocket compatibility

--- src/scs_host/comms/domain_socket.py
+++ src/scs_host/comms/domain_socket.py
@@ -30,7 +30,7 @@ class DomainSocket(ProcessComms):
     __BACKLOG = 1                           # number of unaccepted connections before refusing new connections
     __BUFFER_SIZE = 1024
 
-    __WAIT_FOR_AVAILABILITY =   60.0        # seconds
+    __WAIT_FOR_AVAILABILITY =   120.0       # seconds
 
     # ----------------------------------------------------------------------------------------------------------------
 
